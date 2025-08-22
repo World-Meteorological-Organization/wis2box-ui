7c34831f74ec87281d6d2540ccb92a5c8eb0c6aa
Maaike <mlimper@wmo.int>
2025-08-22 13:42:00 +0200

    Support other links (#119)
    * support non-real-time data
    
    * add label for links with rel=data
    
    * non language specific
    
    * support data/archives and license link
    
    * external-data-link
    
    * change license icon
    
    * add service-desc
    
    * add data
    
    * empty strings in _template.json for consistency
    
    * re-add data and add translations
    
    * update final suggestion from Ben

--- src/views/DatasetView.vue
+++ src/views/DatasetView.vue
@@ -43,7 +43,8 @@
         <v-col>
           <span>
             <strong>{{ $t("datasets.topic") + ": " }}</strong>
-            <code>{{ dataset.properties['wmo:topicHierarchy'] }}</code>
+            <code v-if="dataset.properties['wmo:topicHierarchy']">{{ dataset.properties['wmo:topicHierarchy'] }}</code>
+            <code v-else>---</code>
             <br>
             <strong>{{ $t("datasets.metadata_id") + ": " }}</strong>
             <code>{{ dataset.properties.id }}</code>
@@ -118,7 +119,13 @@ export default defineComponent({
 
         for (const feature of data.features) {
 
-          const hasSynop = feature.properties["wmo:topicHierarchy"].includes("surface-based-observations/synop");
+          // if "wmo:topicHierarchy" not is available in the properties hasSynop should be false
+          let hasSynop = false;
+          let hasTopic = false;
+          if (feature.properties["wmo:topicHierarchy"]) {
+            hasTopic = true;
+            hasSynop = feature.properties["wmo:topicHierarchy"].includes("surface-based-observations/synop");
+          }
           const uiLinks = [];
 
           if (hasSynop) {
@@ -147,17 +154,34 @@ export default defineComponent({
                 msg: "oafeat",
                 icon: "mdi-database-search",
               });
+            } else if (["data", "archives", "service-desc"].includes(link.rel)) {
+              uiLinks.push({
+                href: link.href,
+                target: undefined,
+                type: "external-data-link",
+                msg: "external-data-link",
+                icon: "mdi-file-document"
+              });
+            } else if (link.rel === "license") {
+              uiLinks.push({
+                href: link.href,
+                target: undefined,
+                type: "license",
+                msg: "license",
+                icon: "mdi-license"
+              });
             }
           }
 
-          uiLinks.push({
-            target: undefined,
-            href: `${window.VUE_APP_OAPI}/collections/messages/items?metadata_id=${feature.id}`,
-            type: "Info",
-            msg: "messages",
-            icon: "mdi-message-text-outline",
-          });
-
+          if(hasTopic) {
+            uiLinks.push({
+              target: undefined,
+              href: `${window.VUE_APP_OAPI}/collections/messages/items?metadata_id=${feature.id}`,
+              type: "Info",
+              msg: "messages",
+              icon: "mdi-message-text-outline",
+            });
+          }
           const bbox = feature.geometry.coordinates[0].flat(2);
 
           this.datasets.push({
